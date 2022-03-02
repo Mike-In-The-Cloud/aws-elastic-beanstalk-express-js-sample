f769cf5252be48a26c9aeb9fce5e848b95d8d155
Mike-In-The-Cloud <48106132+Mike-In-The-Cloud@users.noreply.github.com>
2022-03-02 16:46:26 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS CI CD Lab!'));
+app.get('/', (req, res) => res.send('Full Pipeline Test with manual review!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
